1457c8a94baaab5efc153bccb97f67eb580768cc
NaveenGuna1218 <37579323+NaveenGuna1218@users.noreply.github.com>
2018-03-20 16:43:06 +0530

    Create set1-1.cpp

---
+++ set1-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int num;
+cout<<"enter a number\n";
+cin>>num;
+if(num>0)
+cout<<"positive";
+else if(num<0)
+cout<<"negative";
+else
+cout<<"zero";
+
+	return 0;
+}
